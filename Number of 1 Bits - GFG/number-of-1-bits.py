24760caf1dff8aab2cf3349637fa40ba55099d1d
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-04-27 13:13:39 +0530

    Added solution - LeetHub

---
+++ Number of 1 Bits - GFG/number-of-1-bits.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+class Solution:
+	def setBits(self, N):
+	    n = bin(N)
+	    count =0
+	    for x in n:
+	        if x =="1":
+	            count+=1
+	    return count
+		# code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		N = int(input())
+		ob = Solution()
+		ans = ob.setBits(N)
+		print(ans)
+
+
+
+
+# } Driver Code Ends
