229f45a12633c41477a457a4b0a7d77508d7dc6c
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-06-18 23:57:00 +0530

    Time: 203 ms (38.95%), Space: 14.7 MB (89.57%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -17,4 +17,6 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
+# Approach 2   Intuition: Instead of traversing through each row and column, we can use dummy arrays to check if the particular row or column has an element 0 or not, which will improve the time complexity.
+
+# Approach:Take two dummy array one of size of row and other of size of column.Now traverse through the array.If matrix[i][j]==0 then set dummy1[i]=0(for row) and dummy2[j]=0(for column).Now traverse through the array again and if dummy1[i]==0  || dummy2[j]==0 then arr[i][j]=0,else continue.
