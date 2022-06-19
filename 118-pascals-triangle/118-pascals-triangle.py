2d28ba23cc82037947ad3f7509d5c9ba34b91188
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-06-19 17:12:57 +0530

    Time: 47 ms (45.52%), Space: 13.9 MB (17.71%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        matrix = []
+        for r in range(numRows):
+            arr = list()
+            if r == 0:
+                arr = [0]*(r+1)
+                arr[0] = 1
+            elif r == 1:
+                arr = [0]*(r+1)
+                arr[0]=1
+                arr[-1]=1
+            else:
+                arr = [0]*(r+1)
+                arr[0]=1
+                arr[-1]=1
+                for j in range(1,r):
+                    arr[j] = matrix[r-1][j-1]+matrix[r-1][j]
+                    
+            matrix.append(arr)
+        print(matrix)
+        return matrix
+                
