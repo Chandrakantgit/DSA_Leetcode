3c79b759c6da40bba042a18502b0233d1c75bc2a
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-06-27 23:38:59 +0530

    Time: 341 ms (5.03%), Space: 18.1 MB (51.08%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,14 +1,18 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        # THE VERY FIRST THING TO DO IS  SORT THE QUESTION ARRAY OR LIST
+        # THEN TAKE A TEMPORARY PAIR STARTING FROM FIRST ELEMENT OF SORTED ARRAY AND THEN
+        # THEN ITERATE OVER  ARRAY TO CHECK WHETHER PAIR AND CURRENT PAIR COULD BE MERGED
+        # IF CAN'T BE MERGED THEN YOU HAVE TO ADD THAT PAIR IN ANS ARRAY ELSE MERGE ARRAY
+        
         intervals = sorted(intervals)
-        print(intervals)
         ans = []
         pair = intervals[0]
         for x in intervals:
             if x[0] <= pair[1]:
                 pair[0] = min(pair[0],x[0])
                 pair[1] = max(pair[1],x[1])
-                print(pair)
             else:
                 ans.append(pair)
                 pair = x
