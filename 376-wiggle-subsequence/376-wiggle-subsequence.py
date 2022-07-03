ddc0c67ffc14657091c163ca25644de0ae16540f
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-07-03 23:53:58 +0530

    Time: 59 ms (46.46%), Space: 13.9 MB (77.55%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -8,15 +8,12 @@ class Solution:
                 ans.append(x)
                 flag = True
                 temp = x
-                print("here")
             elif flag == True and x < temp:
                 ans.append(x)
                 flag = False
                 temp = x
-                print("here2")
             else:
                 temp = x
-                print("here3")
         
         temp = nums[0]
         ans1=[nums[0]]
@@ -26,15 +23,12 @@ class Solution:
                 ans1.append(x)
                 flag = True
                 temp = x
-                print("here")
             elif flag == True and x > temp:
                 ans1.append(x)
                 flag = False
                 temp = x
-                print("here2")
             else:
                 temp = x
-                print("here3")
         if len(ans) > len(ans1):
             return len(ans)
         else:
