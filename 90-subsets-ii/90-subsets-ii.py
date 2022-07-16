d48193edcd8be188f1c4942d982d6aacbb9d99dd
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-07-16 23:53:28 +0530

    Time: 101 ms (5.19%), Space: 14.1 MB (48.82%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -5,6 +5,9 @@ class Solution:
         self.findSubset(0,nums,[],ansList)
         return ansList
     
+    
+    ##Optimized form of SUbset I , Tiem complexity : 2^n * n ,Space complexity: 2^n , Auxiliary space for recursion : O (n)
+    
     def findSubset(self,ind:int,nums:List[int],ds:List[int],ansList:List[List[int]]):
         tempArr =[x for x in ds]
         ansList.append(tempArr)
