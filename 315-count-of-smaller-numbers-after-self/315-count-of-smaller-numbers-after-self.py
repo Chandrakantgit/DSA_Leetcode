a10dd4b80f2a636c5113d3e5649f48a6158e521d
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-07-24 00:39:43 +0530

    Time: 3003 ms (81.89%), Space: 32.5 MB (81.15%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -4,14 +4,16 @@ class Solution:
     
     def countSmaller(self, nums: List[int]) -> List[int]:
         
+        # bisect.bisect_left(a, x, lo=0, hi=len(a), *, key=None)
+# Locate the insertion point for x in a to maintain sorted order. The parameters lo and hi may be used to specify a subset of the list which should be considered; by default the entire list is used. If x is already present in a, the insertion point will be before (to the left of) any existing entries. The return value is suitable for use as the first parameter to list.insert() assuming that a is already sorted.
+        
         arr,ans = sorted(nums),[]
         for num in nums:
+            i = bisect_left(arr,num)          
+            ans.append(i)
+            del arr[i]
             
-            i = bisect_left(arr,num)    
-            ans.append(i)               
-            del arr[i]                  
-            
-        return ans                    
+        return ans
         
         
         
