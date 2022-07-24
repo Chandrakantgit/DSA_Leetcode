3f145e64609297d57d0f51b3288ca03b5e36ef76
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-07-24 16:26:54 +0530

    Time: 321 ms (22.55%), Space: 20.4 MB (40.22%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,18 +1,31 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        #This is Brute force approach
+        # m = len(matrix)
+        # n = len(matrix[0])
+        # i = 0
+        # j = 0
+        # flag = 0
+        # while i < m:
+        #     for x in range(n):
+        #         if matrix[i][x] == target:
+        #             return True
+        #         elif matrix[i][x] > target:
+        #             n=x
+        #             break
+        #     i+=1
+        
         m = len(matrix)
         n = len(matrix[0])
-        i = 0
         j = 0
-        flag = 0
-        while i < m:
-            for x in range(n):
-                if matrix[i][x] == target:
-                    return True
-                elif matrix[i][x] > target:
-                    n=x
-                    break
-            i+=1
-                        
+        i = m-1
+        while i >-1 and j < n:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                j+=1
+            elif matrix[i][j] > target:
+                i-=1
+        else: return False
         
         
