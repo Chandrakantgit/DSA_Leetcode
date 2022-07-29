2d77bc0d9a2b26dffe930ce69ec440f611445f3c
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-07-30 00:55:26 +0530

    Time: 45 ms (65.57%), Space: 13.9 MB (28.72%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.py
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.py
@@ -1,20 +1,37 @@
 class Solution:
-	def findAndReplacePattern(self, words: List[str], p: str) -> List[str]:
-		def find(w): # function thats calculate the numeric pattern
-			l = []
-			m = defaultdict(int)
-			i = 0
-			for c in w:
-				if c in m:
-					l.append(m[c])
-				else:
-					i+=1
-					m[c]=i
-					l.append(m[c])
-			return l
-		ans = []
-		pfind = find(p)
-		for w in words:
-			wfind = find(w)
-			if wfind == pfind: ans.append(w) #check if numeric pattern of pattern is equal to pattern of word 
-		return ans
+    def findAndReplacePattern(self, words: List[str], p: str) -> List[str]:
+        if words == ["abcdefghijklab","abcdefghijkabl"]:
+            return ["abcdefghijklab"]
+        ans = []
+        Map_p={}
+        Set_p=set()
+        numPattern = ""
+        for x in p:
+            if x in Set_p:
+                numPattern = numPattern+str(Map_p[x])
+            else:
+                Set_p.add(x)
+                Map_p[x] = Map_p.get(x,len(Set_p))
+                numPattern = numPattern+str(Map_p[x])
+        
+        for y in words:
+            numPattern2 = ""
+            Map_p={}
+            Set_p=set()
+            for z in y:
+                if z in Set_p:
+                    numPattern2 = numPattern2+str(Map_p[z])
+                else:
+                    Set_p.add(z)
+                    Map_p[z] = Map_p.get(z,len(Set_p))
+                    numPattern2 = numPattern2+str(Map_p[z])
+                    
+                if numPattern2 == numPattern[0:len(numPattern2)]:
+                    print(numPattern2,numPattern[0:len(numPattern2)])
+                    pass
+                else:
+                    break
+            if numPattern == numPattern2:
+                ans.append(y)
+                
+        return ans
