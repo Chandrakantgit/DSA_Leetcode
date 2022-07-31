86c9bd51f8811e53a0f579abb4b84ba2e2192ae5
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-07-31 15:28:13 +0530

    Time: 117 ms (45.43%), Space: 26.3 MB (63.87%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,7 +7,12 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        #have to redo it
+        # Applied DFS, goes deep inside the tree to find out p or q.
+        #the moment we find p or q,it is returned. if both p and q are found hen the root at which they are found i returned.
+        # IT IS A RECURSIVE SOLUTION.
+        
+        # Time complexity : O(n)
+        # Auxiliary space complexity : O(n) (due tpo recursion stack)
         if root == None or root == p or root == q:
             return root
         left = self.lowestCommonAncestor(root.left,p,q)
