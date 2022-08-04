9b69b40b7ff7d2a572c69e79ae8ade5d499941b3
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-08-05 01:59:27 +0530

    Time: 66 ms (44.25%), Space: 14 MB (84.46%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,21 +1,47 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        
-        def recurPermute(ind,arr,ans):
-            if ind ==  len(arr):
+#         More optimized solution 
+
+#         def recurPermute(ind,arr,ans):
+#             if ind ==  len(arr):
+#                 ar = list()
+#                 for x in arr:
+#                     ar.append(x)
+#                 ans.append(ar)
+#                 return
+#             for j in range(ind,len(nums)):
+#                 arr[ind],arr[j] = arr[j],arr[ind]
+#                 recurPermute(ind+1,arr,ans)
+#                 arr[ind],arr[j]=arr[j],arr[ind]
+                
+#         def caller(num):
+#             ans = list()
+#             recurPermute(0,num,ans)
+#             return ans
+            
+#         return caller(nums)
+
+
+        def recurPermute(ds,arr,ans,freq):
+            if len(ds) ==  len(arr):
                 ar = list()
-                for x in arr:
+                for x in ds:
                     ar.append(x)
                 ans.append(ar)
                 return
-            for j in range(ind,len(nums)):
-                arr[ind],arr[j] = arr[j],arr[ind]
-                recurPermute(ind+1,arr,ans)
-                arr[ind],arr[j]=arr[j],arr[ind]
+            for j in range(0,len(nums)):
+                if not freq[j]:
+                    freq[j]=True
+                    ds.append(nums[j])
+                    recurPermute(ds,arr,ans,freq)
+                    ds.pop()
+                    freq[j]=False
                 
         def caller(num):
             ans = list()
-            recurPermute(0,num,ans)
+            freq = [False]*len(nums)
+            recurPermute([],num,ans,freq)
             return ans
             
         return caller(nums)
+
