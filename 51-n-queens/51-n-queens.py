54e945ce58f9d9f6e0bab86d51acd09940d45ddd
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-08-07 01:07:00 +0530

    Time: 196 ms (17.58%), Space: 14.4 MB (82.60%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -1,21 +1,27 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         
+        # Recursive solution
+        
+        #Function to check whether the placed queen is safe from other queens or not
         def isSafe(row,col,board,n):
             duplicateRow = row
             duplicateCol = col
             
+            #for checking diagonally
             while row >=0 and col >=0:
                 if board[row][col] == "Q": return False
                 row -= 1
                 col -= 1
             
+            #for checking column wise
             row = duplicateRow
             col = duplicateCol 
             while col >= 0:
                 if board[row][col] == "Q": return False
                 col -= 1
                 
+            #for checking row wise
             row = duplicateRow
             col = duplicateCol 
             while row < n and col >= 0:
@@ -38,7 +44,7 @@ class Solution:
             for y in range(0,n):
                 if isSafe(y,col,board,n):
                     board[y] = board[y][0:col]+"Q"+board[y][col+1:]
-                    solve(col+1,board,ans,n)
+                    solve(col+1,board,ans,n)   #after this that Queen has to be removed
                     board[y] = board[y][0:col]+"."+board[y][col+1:]
                     # board[y][col] = "."
         
