6d9e8295ee0c7410f2e18ed6fcfef6ae192a9c7c
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-08-23 22:44:01 +0530

    Time: 1618 ms (11.00%), Space: 47.1 MB (15.86%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -10,14 +10,11 @@ class Solution:
         while temp:
             s=s+str(temp.val)
             temp = temp.next
-        print(s)
         i = 0
         j = len(s)-1-i
         while i<j:
-            print(s[i],s[j])
             if s[i] != s[j]:
                 return False
-            print("here")
             i+=1
             j-=1
         return True
