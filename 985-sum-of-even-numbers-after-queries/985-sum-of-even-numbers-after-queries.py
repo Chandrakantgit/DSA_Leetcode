066671e5f5857dc70a6f2e4f838db097351fff22
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-09-21 11:54:55 +0530

    Time: 1734 ms (5.10%), Space: 20.6 MB (43.88%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
@@ -4,35 +4,29 @@ class Solution:
         for x in nums:
             if x%2 ==0:
                 evenSum += x
-        print(evenSum)
         ans = []
         for a in queries:
             
             if nums[a[1]] % 2 ==0:
                 temp = nums[a[1]] + a[0]
-                print("here temp",temp,nums[a[1]])
                 
                 if temp < nums[a[1]] and temp %2 ==0:
                     evenSum -= (nums[a[1]] - temp)
                     nums[a[1]] = temp
-                    print(temp,1)
                     ans.append(evenSum)
                 elif temp >= nums[a[1]] and temp %2 ==0:
                     evenSum += (temp - nums[a[1]])
                     nums[a[1]] = temp
-                    print(temp,2)
                     ans.append(evenSum)
                 else:
                     evenSum -= nums[a[1]]
                     nums[a[1]] = temp
-                    print(temp,3)
                     ans.append(evenSum)  
             else:
                 temp = (nums[a[1]]) + (a[0])
                 if temp %2 == 0:
                     evenSum += temp
                     nums[a[1]] = temp
-                    print(temp,4)
                     ans.append(evenSum)
                 else:
                     nums[a[1]] = temp
