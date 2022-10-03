06b066188d1ac3db12e8032d3d672ab123300de8
Chandrakant_Khannade <59232673+Chandrakantgit@users.noreply.github.com>
2022-10-03 17:46:33 +0530

    Time: 7549 ms (5.05%), Space: 24.8 MB (82.67%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -5,25 +5,8 @@ class Solution:
         count = 0
         ans = 0
         for x in range(0,lenC):
-            try:
-                if x == lenC-1:
-                    if colors[x] == colors[x-1]:
-                        temp.append(neededTime[x])
-                        count+=1
-                        temp = sorted(temp)
-                        if len(temp) > 1:
-                            for i in range(0,count-1):
-                                ans+=temp.pop(0)
-                            temp = []
-                            count = 0
-                        else:
-                            temp = []
-                            count = 0
-                        
-                elif colors[x] == colors[x+1]:
-                    temp.append(neededTime[x])
-                    count += 1
-                else:
+            if x == lenC-1:
+                if colors[x] == colors[x-1]:
                     temp.append(neededTime[x])
                     count+=1
                     temp = sorted(temp)
@@ -31,11 +14,26 @@ class Solution:
                         for i in range(0,count-1):
                             ans+=temp.pop(0)
                         temp = []
-                        count=0
+                        count = 0
                     else:
                         temp = []
-                        count=0
-            except:pass
+                        count = 0
+                        
+            elif colors[x] == colors[x+1]:
+                temp.append(neededTime[x])
+                count += 1
+            else:
+                temp.append(neededTime[x])
+                count+=1
+                temp = sorted(temp)
+                if len(temp) > 1:
+                    for i in range(0,count-1):
+                        ans+=temp.pop(0)
+                    temp = []
+                    count=0
+                else:
+                    temp = []
+                    count=0
         return ans
                     
                 
